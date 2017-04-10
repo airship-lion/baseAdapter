1d970f5103afe72f4ee0f5df2ee156e6b7a6b55c
airship-lion <1962619275@qq.com>
2017-04-10 16:20:51 +0800

    测试提交github

--- sample/src/main/java/com/zhy/sample/MainActivity.java
+++ sample/src/main/java/com/zhy/sample/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.zhy.adapter.abslistview.CommonAdapter;
 import com.zhy.adapter.abslistview.ViewHolder;
@@ -29,7 +30,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
         mListView = ((ListView) findViewById(R.id.id_listview_list));
         mEmptyView = findViewById(R.id.id_empty_view);
         mListView.setAdapter(new CommonAdapter<String>(this, R.layout.item_list, mDatas)
